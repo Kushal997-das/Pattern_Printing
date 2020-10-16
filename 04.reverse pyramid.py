122e3a4168ac6ea381adfbb496d5be4252cc7779
Kushal Das <61356005+Kushal997-das@users.noreply.github.com>
2020-10-16 09:47:06 +0530

    add simple method

--- 04.reverse pyramid.py
+++ 04.reverse pyramid.py
@@ -13,3 +13,7 @@ for i in range(n,0,-1): #for rows
     for j in range(0,i): # creating star
         print("*",end=' ')
     print()
+#other method
+n=int(intput()
+for i in range(n, 0, -1):
+    print(' ' * (n - i) + '* ' * (i))
