1f0f1723a2348ac4ccde85d1f2deba20c24ef5e5
kushal <daskushal980@gmail.com>
2020-10-17 10:42:10 +0530

    Added A pattern

--- 07.Printing_A.py
+++ 07.Printing_A.py
@@ -1,7 +1,17 @@
-
-for row in range(7):
-    for col in range(5):
-        if ((col==0 or col==4) and (row!=0)) or ((row==0 or row==3) and (col>0 and col<4)):
+'''
+ ***
+*   *
+*   *
+*****
+*   *
+*   *
+*   *
+'''
+n=int(input("enter the row"))
+m=int(input("enter the column"))
+for row in range(n):
+    for col in range(m):
+        if ((col==0 or col==m-1) and (row!=0)) or ((row==0 or row==n-4) and (col>0 and col<m-1)):
             print('*',end='')
         else:
             print(end=' ')
