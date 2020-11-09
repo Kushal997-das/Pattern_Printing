cd4c39aa79d8aae2a2aa5aea1029a7af62724806
kushal <daskushal980@gmail.com>
2020-11-09 22:16:40 +0530

    Added Stars '*' in E Shape

---
+++ 11.Printing Stars in E Shape.py
@@ -0,0 +1,16 @@
+'''
+*****
+*
+*
+*****
+*
+*
+*****
+'''
+for row in range(7):
+    for col in range(5):
+        if (col==0) or ((row==0 or row==3 or row==6) and (col>0)):
+            print('*',end='')
+        else:
+            print(end=' ')
+    print()
