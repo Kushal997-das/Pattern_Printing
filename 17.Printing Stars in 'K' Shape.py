330325cbd3f830b5473fb100a781082ad65438e0
kushal <daskushal980@gmail.com>
2020-11-12 22:28:10 +0530

    added stars in K shape!

--- 17.Printing Stars in 'K' Shape.py
+++ 17.Printing Stars in 'K' Shape.py
@@ -1,3 +1,10 @@
+'''
+*   *
+* *
+**
+* *
+*   *
+'''
 for row in range(5):
     for col in range(5):
         if(col==0) or (row==0 and col==4) or (row==1 and (col>1 and col<3)) or (row==2 and (col>0 and col<2)) or (row==3 and (col>1 and col<3)) or (row==4 and (col==4)):
