2b5411535957d1ee943ee580571578e497c9e285
kushal <daskushal980@gmail.com>
2020-11-12 10:55:15 +0530

    added printing 'F' !

--- 12.Printing stars in F Shape.py
+++ 12.Printing stars in F Shape.py
@@ -1,16 +1,17 @@
 #Printing Stars in 'F' Shape !
 '''
-*****
+* * * * *
 *
 *
-****
+* * * *
 *
 *
+*     
 '''
-for row in range(6):
+for row in range(7):
     for col in range(5):
         if (col==0)or((row==0) and (col>0) or (row==3)and(col>0 and col<4)):
-            print('*',end='')
+            print('*',end=' ')
         else:
             print(end=' ')
     print()
