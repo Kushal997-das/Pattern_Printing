44bf89a6607af0508cf6ab0c43212595bac07175
kushal <daskushal980@gmail.com>
2020-11-12 17:11:19 +0530

    added stars in J shape!

---
+++ 16.Printing Stars in 'J' Shape.py
@@ -0,0 +1,8 @@
+
+for row in range(7):
+    for col in range(5):
+        if(row==0) or (row==6 and col<2) or (col==2):
+            print('*',end='')
+        else:
+            print(end=' ')
+    print()
