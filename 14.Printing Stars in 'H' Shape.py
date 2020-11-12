30e300eae8619afefa57c71982807a36981704fb
kushal <daskushal980@gmail.com>
2020-11-12 16:39:35 +0530

    added stars in H shape!

---
+++ 14.Printing Stars in 'H' Shape.py
@@ -0,0 +1,7 @@
+for row in range(7):
+    for col in range(5):
+        if(col==0 or col==4) or (row==3 and col<4):
+            print('*',end='')
+        else:
+            print(end=' ')
+    print()
