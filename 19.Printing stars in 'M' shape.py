4eddaf000520fe132906d8e16c77cdf4b06e0159
kushal <daskushal980@gmail.com>
2020-11-12 23:23:40 +0530

    added stars in M shape!

--- 19.Printing stars in 'M' shape.py
+++ 19.Printing stars in 'M' shape.py
@@ -1,3 +1,12 @@
+'''
+*     *
+**   **
+* * * *
+*  *  *
+*     *
+*     *
+*     *
+'''
 for row in range(7):
     for col in range(7):
         if (col==0 or col==6 ) or (row==col and(col>0 and col<4)) or (row==1 and col==5)or(row==2 and col==4):
