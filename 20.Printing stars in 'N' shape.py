31e3308c8f270b0b88757ae5e411ed2363d141c5
kushal <daskushal980@gmail.com>
2020-11-13 12:01:50 +0530

    added stars in N shape!

--- 20.Printing stars in 'N' shape.py
+++ 20.Printing stars in 'N' shape.py
@@ -1,6 +1,6 @@
 for row in range(7):
     for col in range(7):
-        if(col==0 or col==6) or(row==col and(col>0 and col<7)):
+        if(col==0 or col==6) or(row==col and(col>0 and col<6)):
             print('*', end='')
         else:
             print(end=' ')
