42d30bc8b0640fdeaaff9a0731e458739b671a5e
kushal <daskushal980@gmail.com>
2020-11-17 09:59:04 +0530

    added stars in P shape!

---
+++ 22.Printing stars in 'P' shape.py
@@ -0,0 +1,7 @@
+for row in range(6):
+    for col in range(6):
+        if col==0  or (col==5 and (row==1 or row==2)) or (row==0 or row==3)and(col>0 and col<4):
+            print('*', end='')
+        else:
+            print(end=' ')
+    print()
