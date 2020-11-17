23c779602770c6df407388bb8f683b9d844498da
kushal <daskushal980@gmail.com>
2020-11-17 10:00:01 +0530

    added stars in P shape!

--- 22.Printing stars in 'P' shape.py
+++ 22.Printing stars in 'P' shape.py
@@ -1,4 +1,13 @@
 #Printing stars in P shape!
+'''
+****
+*    *
+*    *
+****
+*
+*
+
+'''
 for row in range(6):
     for col in range(6):
         if col==0  or (col==5 and (row==1 or row==2)) or (row==0 or row==3)and(col>0 and col<4):
