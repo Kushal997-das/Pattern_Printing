7c4747566c4bacb376b8ede293bd3804812a7d85
kushal <daskushal980@gmail.com>
2020-11-22 11:47:53 +0530

    added stars in Q shape!

--- 23.Printing stars in 'Q' shape.py
+++ 23.Printing stars in 'Q' shape.py
@@ -1,4 +1,12 @@
 #Printing stars in Q shape!
+'''
+ ***
+*   *
+*   *
+*   *
+ ***
+    *
+'''
 for row in range(6):
     for col in range(5):
         if((col==0 or col==4) and (row>0 and row<4)) or (row==0 or row==4) and (col>0 and col<4) or (col==4 and row==5):
