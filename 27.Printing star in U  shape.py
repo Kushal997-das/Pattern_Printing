3dbc791f29afab5341c07c123a9a812e019d9375
kushal <daskushal980@gmail.com>
2020-11-28 11:42:31 +0530

    added star in 'U' shape

--- 27.Printing star in U  shape.py
+++ 27.Printing star in U  shape.py
@@ -1,6 +1,6 @@
 for row in range(5):
     for col in range(5):
-        if(row==4) or (col==0 or col==4 ) and (row<4):
+        if(col==0 or col==4 ) or (row==4 and col>0 and col <4):
             print('*',end='')
         else:
             print(end=' ')
