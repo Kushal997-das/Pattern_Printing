87e080cb82de593354df4abad827da63b86e2c4b
kushal <daskushal980@gmail.com>
2020-11-28 21:02:28 +0530

    printing star in 'v' pattern

--- 28.Printing star in V shape.py
+++ 28.Printing star in V shape.py
@@ -1,8 +1,13 @@
-
-for row in range(5):
-    for col in range(5):
+i=0
+j=6
+for row in range(4):
+    for col in range(7):
         if(row==col):
             print('*',end='')
+        elif row==i and col==j:
+            print('*',end='')
+            i+=1
+            j-=1
         else:
             print(end=' ')
     print()
