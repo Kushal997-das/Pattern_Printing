6b930b02b76cf73ce74f3532f48c89b2153a3685
kushal <daskushal980@gmail.com>
2020-11-28 20:23:57 +0530

    printing star in 'u' pattern

--- 27.Printing star in U  shape.py
+++ 27.Printing star in U  shape.py
@@ -1,3 +1,11 @@
+#Printing stars in 'U' shape!
+'''
+*   *
+*   *
+*   *
+*   *
+ ***
+'''
 for row in range(5):
     for col in range(5):
         if(col==0 or col==4) and( row!=4) or (row==4 and col>0 and col <4):
