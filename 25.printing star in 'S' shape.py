03880d528f95abfbb95f521916b4dc0ad131c1e8
kushal <daskushal980@gmail.com>
2020-11-28 11:06:37 +0530

    added stars in S shape!

---
+++ 25.printing star in 'S' shape.py
@@ -0,0 +1,7 @@
+for row in range(7):
+    for col in range(5):
+        if(row==0 or row==3 or row==6) and (col>0 and col<4) or (col==0 and row>0 and row<3) or (col==4 and row>3 and row<6):
+            print('*', end='')
+        else:
+            print(end=' ')
+    print()
