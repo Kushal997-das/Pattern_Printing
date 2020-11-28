81fa0bbf0d75916b5ea1fca603007c6626fd922e
kushal <daskushal980@gmail.com>
2020-11-28 10:42:08 +0530

    added stars in R shape!

---
+++ 24.Printing stars in'R' shape.py
@@ -0,0 +1,7 @@
+for row in range(7):
+    for col in range(5):
+        if (col==0) or (row==0 or row==3)and (col<4) or (col==4)and (row!=0 and row!=3):
+            print('*',end='')
+        else:
+            print(end=' ')
+    print()
