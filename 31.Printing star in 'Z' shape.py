ac04b5430c36d55d1b4ad12a280c9c79090892c8
kushal <daskushal980@gmail.com>
2020-12-02 13:05:11 +0530

    Added star in Z shape

---
+++ 31.Printing star in 'Z' shape.py
@@ -0,0 +1,13 @@
+i=1
+j=3
+for row in range(5):
+    for col in range(5):
+        if(row==0)or(row==4):
+            print('*',end='')
+        elif (row==i and col==j):
+            print('*', end='')
+            i+=1
+            j-=1
+        else:
+            print(end=' ')
+    print()
