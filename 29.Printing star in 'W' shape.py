c6ef61e81e2d11dc1b7fc8a8f13c3e660c218edd
kushal <daskushal980@gmail.com>
2020-12-02 10:59:10 +0530

    Added 'w'

--- 29.Printing star in 'W' shape.py
+++ 29.Printing star in 'W' shape.py
@@ -1,7 +1,13 @@
-for row in range(7):
-    for col in range(5):
-        if(col==row):
-            print('*',end=' ')
+i=0
+j=3
+for row in range(4):
+    for col in range(7):
+        if(col==0 or col==6) or (col==4 and row==1) or (col==5 and row==2):
+            print('*',end='')
+        elif row==i and col==j:
+            print('*', end='')
+            i+=1
+            j-=1
         else:
             print(end=' ')
     print()
