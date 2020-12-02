166481099237d4a80afe88abb3b3fe42ed6f3889
kushal <daskushal980@gmail.com>
2020-12-02 12:49:18 +0530

    Added star in Y shape

--- 31.Printing star in 'Y' shape.py
+++ 31.Printing star in 'Y' shape.py
@@ -1,7 +1,15 @@
+i = 0
+j = 4
+
 for row in range(5):
     for col in range(5):
-        if (col==row and col<3):
+
+        if (col==row and col<3) or (col==2 and row>2):
             print("*",end='')
+        elif row==i and col==j and row<2  :
+            print("*", end='')
+            i+=1
+            j-=1
         else:
             print(end=' ')
     print()
