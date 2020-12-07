ccc1bd508d335d8fb91d2d81ab24fbbc040b6ebb
kushal <daskushal980@gmail.com>
2020-12-07 11:02:25 +0530

    Added hollow diamond

---
+++ Important printing pattern/05.Hollowdiamondshape.py
@@ -0,0 +1,7 @@
+for row in range(5):
+    for col in range(5):
+        if (row+col==2) or (row+col==6) or (row==1 and col==3) or (row==3 and col==1):
+            print("*",end='')
+        else:
+            print(end=' ')
+    print()
