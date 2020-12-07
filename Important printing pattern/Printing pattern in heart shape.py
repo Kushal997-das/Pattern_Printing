b8d73523d96c8a85433b1bc44bdc2dfe5b237d50
kushal <daskushal980@gmail.com>
2020-12-07 11:38:54 +0530

    Added Heart shape

--- Important printing pattern/Printing pattern in heart shape.py
+++ Important printing pattern/Printing pattern in heart shape.py
@@ -1,3 +1,11 @@
+'''
+ ** **
+*  *  *
+*     *
+ *   *
+  * *
+   *   
+'''
 for row in range(6):
     for col in range(7):
         if(row==0 and col%3!=0) or (row==1 and col%3==0) or row-col==2 or row+col==8:
