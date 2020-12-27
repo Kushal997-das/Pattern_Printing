773b4d3eb48ecaefd1e22e3ffdff0751b55c9859
kushal <daskushal980@gmail.com>
2020-12-27 20:41:57 +0530

    Added numbers in pyramid shape

--- Important printing pattern/Printing Numbers in Pyramid Shape.py
+++ Important printing pattern/Printing Numbers in Pyramid Shape.py
@@ -1,3 +1,11 @@
+'''
+enter the number:5
+          1
+        2 1 2
+      3 2 1 2 3
+    4 3 2 1 2 3 4
+  5 4 3 2 1 2 3 4 5
+'''
 num=int(input('enter the number:'))
 for i in range(1,num+1):
     for j in range(0,num-i+1):
