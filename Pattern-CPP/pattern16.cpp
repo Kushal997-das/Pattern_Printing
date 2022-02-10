58b05e98bc2814794debe8427111d0d8df2b663b
Kushal Das <daskushal980@gmail.com>
2022-02-10 21:02:20 +0530

    added new patterns

--- Pattern-CPP/pattern16.cpp
+++ Pattern-CPP/pattern16.cpp
@@ -9,15 +9,18 @@ using namespace std;
 int main()
 {
     int row=1,n;
-    cout<<"enter number";
+    cout<<"enter number"; //char ch='A'+n-row;
     cin>>n;
     while(row<=n){
         int col=1;
-        char ch='A';
+        char ch='A'+row-1; // one way
+      // char ch='A'; // second way
         while(col<=n)
         {
-            cout<<char(ch+row+col-2)<<" ";
+           // cout<<char(ch+row+col-2)<<" "; // second way
+           cout<<ch<<" "; // one way
             col++;
+            ch++;
         }
         row++;
         cout<<endl;
---
+++ Pattern-CPP/pattern23.cpp
@@ -0,0 +1,31 @@
+/*
+   *
+  **
+ ***
+****
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,row=1;
+    cout<<"enter number";
+    cin>>n;
+    while(row<=n)
+    {
+        int space=n-row;
+        while(space)
+        {
+            cout<<" ";
+            space--;
+        }
+        int col=1;
+        while(col<=row)
+        {
+            cout<<"*";
+            col++;
+        }
+        cout<<endl;
+        row++;
+    }
+}
