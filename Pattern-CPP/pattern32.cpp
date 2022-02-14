14b2720c13cf020cce5459bbdb120d2bed5055f1
Kushal Das <daskushal980@gmail.com>
2022-02-14 09:32:50 +0530

    added patterns

--- Pattern-CPP/pattern32.cpp
+++ Pattern-CPP/pattern32.cpp
@@ -24,10 +24,11 @@ int col=n;
               // cout<<col;
             }
             else{
-                cout << col;
+               cout << col;
             }
-            col--;
         
+            col--;
+            
 
         }
 
