ff295cd6d6c76ae387a0a7826863fde4f2aea4b9
Kushal Das <daskushal980@gmail.com>
2022-07-07 16:32:01 +0530

    Update 04.reverse pyramid.py

--- Pattern(Python)/04.reverse pyramid.py
+++ Pattern(Python)/04.reverse pyramid.py
@@ -8,10 +8,12 @@
 '''
 n=int(input())
 for i in range(n,0,-1): #for rows
-    for j in range(0,n-i): # for space
+   
+    for j in range(0,i): # for space
         print(end=' ')
-    for j in range(0,i): # creating star
+    for j in range(0,n-i): # creating star
         print("*",end=' ')
+     
     print()
 #other method
 n=int(input())
